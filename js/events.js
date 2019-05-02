33d708073640314bf646f0088004fe24a8df55d3
Min Kang <kangm914@gmail.com>
2019-05-02 16:13:30 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -19,9 +19,9 @@ function pressIt(){
 }
 
 function submitIt(){
-  $("form").on('submit', function()){
+  $("form").on('submit', function(){
     alert("Your form is going to be submitted now.")
-  })
+  });
 }
 
 $(document).ready(function(){
