87c276e28d28ad980f3481db20e0e0155932a7c6
Min Kang <kangm914@gmail.com>
2019-05-02 16:11:16 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,24 @@
-//define functions here
+function getIt () {
+  $('p').on('click',function(){
+  alert('Hey!')
+});
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt(){
+  $("form").on('keydown', function(key){
+    if (key.which==g){
+      alert('g was pressed');
+    }
+  });
+}
+
+
 
 $(document).ready(function(){
 
